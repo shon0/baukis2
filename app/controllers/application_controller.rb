c6da91f2d4420c3c70824298d70b5101916c8973
shon0 <shono.koga@gmail.com>
2022-01-14 23:30:15 +0900

    add 404 error page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,15 @@
 class ApplicationController < ActionController::Base
   layout :set_layout
 
-  class Forbidden < ActionController::ActionControllerError; end
-  class IpAddressRejected < ActionController::ActionControllerError; end
+  class Forbidden < ActionController::ActionControllerError
+  end
+  class IpAddressRejected < ActionController::ActionControllerError
+  end
 
   rescue_from StandardError, with: :rescue500
   rescue_from Forbidden, with: :rescue403
   rescue_from IpAddressRejected, with: :rescue403
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue404
 
   private def set_layout
     if params[:controller].match(%r{\A(staff|admin|customer)/})
@@ -23,5 +26,9 @@ class ApplicationController < ActionController::Base
   private def rescue403(e)
     @exception = e
     render "errors/forbidden", status: 403
+  end
+
+  private def rescue404(e)
+    render "errors/not_found", status: 404
   end
 end
