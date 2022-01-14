607eca6e8b10426c1c4fea81c2900daebda06915
shon0 <shono.koga@gmail.com>
2022-01-14 22:29:10 +0900

    add 403 error page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   layout :set_layout
 
+  class Forbidden < ActionController::ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionControllerError; end
+
   rescue_from StandardError, with: :rescue500
+  rescue_from Forbidden, with: :rescue403
+  rescue_from IpAddressRejected, with: :rescue403
 
   private def set_layout
     if params[:controller].match(%r{\A(staff|admin|customer)/})
@@ -13,5 +18,10 @@ class ApplicationController < ActionController::Base
 
   private def rescue500(e)
     render "errors/internal_server_error", status: 500
+  end
+
+  private def rescue403(e)
+    @exception = e
+    render "errors/forbidden", status: 403
   end
 end
