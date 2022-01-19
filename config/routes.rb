8048a0f180fbeda8a225d46d8daf230f03fac173
shon0 <shono.koga@gmail.com>
2022-01-19 22:47:13 +0900

    add staff/acount routes & refactor routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
   namespace :staff, path: "" do
     root "top#index"
-    get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create", as: :session
-    delete "session" => "sessions#destroy"
+    get "login" => "sessions#new", :as => :login
+    resource "session", only: %i[create, destroy]
+    resource :account, except: %i[new create destroy]
   end
 
   namespace :admin do
     root "top#index"
-    get "login" => "sessions#new", as: :login
-    post "session" => "sessions#create", as: :session
-    delete "session" => "sessions#destroy"
+    get "login" => "sessions#new", :as => :login
+    resource "session", only: %i[create, destroy]
     resources :staff_members
   end
 
