0bab6388706db7b69b44125b9e03a69ac7d00e94
shon0 <shono.koga@gmail.com>
2022-01-26 21:00:56 +0900

    add brefore_action authorize to admin

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,9 +1,19 @@
 class Admin::Base < ApplicationController
+  before_action :authorize
+
   private def current_administrator
     if session[:administrator_id]
-      @current_administorator ||= Administrator.find_by(id: session[:administrator_id])
+      @current_administorator ||=
+        Administrator.find_by(id: session[:administrator_id])
     end
   end
 
   helper_method :current_administrator
+
+  private def authorize
+    unless current_administrator
+      flash.alert = "管理者としてログインしてください。"
+      redirect_to :admin_login
+    end
+  end
 end
