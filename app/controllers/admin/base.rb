e9cb36f6ae0d8ea9679ab8d9b42b7ceacd5d2e51
shon0 <shono.koga@gmail.com>
2022-01-26 22:33:00 +0900

    add brefore_action of force logout

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,5 +1,6 @@
 class Admin::Base < ApplicationController
   before_action :authorize
+  before_action :check_account
 
   private def current_administrator
     if session[:administrator_id]
@@ -14,6 +15,14 @@ class Admin::Base < ApplicationController
     unless current_administrator
       flash.alert = "管理者としてログインしてください。"
       redirect_to :admin_login
+    end
+  end
+
+  private def check_account
+    if current_administrator && !current_administrator.active?
+      session.delete(:administrator_id)
+      flash.alert = "アカウントが無効になりました。"
+      redirect_to :admin_root
     end
   end
 end
