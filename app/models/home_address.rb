bd3ef722dd042facce3283f86f4b53b93e4e1c83
shon0 <shono.koga@gmail.com>
2022-01-31 22:02:40 +0900

    add customer validates

--- app/models/home_address.rb
+++ app/models/home_address.rb
@@ -1,3 +1,9 @@
 class HomeAddress < Address
-  
+  validates :postal_code,
+            :prefecture,
+            :city,
+            :address1,
+            :address2,
+            presence: true
+  validates :company_name, :division_name, absence: true
 end
